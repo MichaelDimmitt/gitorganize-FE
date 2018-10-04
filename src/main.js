de1eb95f8e696b8663d5bd5d784105687b63fe2c
Michael Dimmitt <michaelgdimmitt@gmail.com>
2018-10-04 01:47:15 -0400

    Rename the helloworld counter to the future PinnScrollbar

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Counter from "./Counter";
+import PinnedScrollbar from "./PinnedScrollbar.js";
 
 document.addEventListener("DOMContentLoaded", function() {
   ReactDOM.render(
-    React.createElement(Counter),
+    React.createElement(PinnedScrollbar),
     document.getElementById("mount")
   );
 });
