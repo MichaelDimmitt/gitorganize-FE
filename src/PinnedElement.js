c2d7a2ffc31f1d4504e5af7296541a2b6d6b6ed5
Michael Dimmitt <michaelgdimmitt@gmail.com>
2018-10-09 12:18:24 -0400

    extract hardcoded values to parent component

--- src/PinnedElement.js
+++ src/PinnedElement.js
@@ -2,38 +2,38 @@ import React from 'react';
 const { HandleThing, RepoTitle, SeemsUnused } = require('./lineOneDependencies.js')
 export { PinnedElement }
 
-const LineOneHandleAndTitle = ({}) => (
+const LineOneHandleAndTitle = ({repo, user}) => (
   <span className="d-block position-relative">
     <input type="hidden" name="repo_ids[]" id="pinned-repo-reorder-147890778" value="147890778" className="form-control" />
-    <HandleThing />
-    <RepoTitle   repo="ERRORSCREAM" user="MichaelDimmitt"/>
-    <SeemsUnused repo="ERRORSCREAM"/>
+    <HandleThing /> {/* At moment seems it does not take data. May need to remove superflous logic. */}
+    <RepoTitle   repo={repo} user={user}/>
+    <SeemsUnused repo={repo}/>
   </span>
 )
 
-const LineTwoDescription = ({name}) => (
+const LineTwoDescription = ({description}) => (
   <p className="pinned-repo-desc text-gray text-small d-block mt-2 mb-3">
-    stdout to error
+    {description}
   </p>
 )
 
-const LineThreeLanguageAndStar = ({name}) => (
+const LineThreeLanguageAndStar = ({repo, user, starCount}) => (
   <p className="mb-0 f6 text-gray">
     <span className="repo-language-color pinned-repo-meta" style={{backgroundColor:"#89e051"}}></span>
     Shell
-    <a href="/MichaelDimmitt/ERRORSCREAM/stargazers" className="pinned-repo-meta muted-link">
+    <a href={`/${user}/${repo}/stargazers`} className="pinned-repo-meta muted-link">
       <svg aria-label="star" className="octicon octicon-star" viewBox="0 0 14 16" version="1.1" width="14"
         height="16" role="img">
         <path d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z" /></svg>
-      1
+      {starCount}
     </a>
   </p>
 )
 
 const PinnedElement = ({}) => (
   <div className="pinned-repo-item-content">
-    <LineOneHandleAndTitle/>
-    <LineTwoDescription/>
-    <LineThreeLanguageAndStar/>
+    <LineOneHandleAndTitle repo="ERRORSCREAM" user="MichaelDimmitt"/>
+    <LineTwoDescription description="stdout to error"/>
+    <LineThreeLanguageAndStar repo="ERRORSCREAM" user="MichaelDimmitt" starCount={1}/>
   </div>
 )
